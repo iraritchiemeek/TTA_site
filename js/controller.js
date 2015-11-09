57984394be971f9e0d0db437709ce8f6ada403ee
iraritchiemeek <iraritchiemeek@gmail.com>
2015-11-09 23:15:18 +1300

    Add strategy headers showing up on hover

--- js/controller.js
+++ js/controller.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
 	
 	var view = new View
 
-	view.addImage('reading.svg', 'strategy_icon', '#strategy_wrapper')
-	view.addImage('todo_list.svg', 'strategy_icon', '#strategy_wrapper')
-	view.addImage('bullish.svg', 'strategy_icon', '#strategy_wrapper')
-	view.addImage('collaboration.svg', 'strategy_icon', '#strategy_wrapper')
-	view.addImage('inspection.svg', 'strategy_icon', '#strategy_wrapper')
+	view.addImage('reading', 'svg', 'strategy_icon', '#strategy_wrapper')
+	view.addImage('todo_list', 'svg', 'strategy_icon', '#strategy_wrapper')
+	view.addImage('bullish', 'svg', 'strategy_icon', '#strategy_wrapper')
+	view.addImage('collaboration', 'svg', 'strategy_icon', '#strategy_wrapper')
+	view.addImage('inspection', 'svg', 'strategy_icon', '#strategy_wrapper')
+
+	$('.strategy_icon').on('mouseenter', function(e){
+		var strategy = e.currentTarget.attributes.id
+		view.displayStrategy(strategy)
+	})
 })
--- js/view.js
+++ js/view.js
@@ -1,7 +1,11 @@
 function View () {
 
-	View.prototype.addImage = function(url, class_name, target) {
-		console.log($(target))
-		$(target).append('<img src="imgs/' + url + '" class="' + class_name + '"/>')
+	View.prototype.addImage = function(image_name, suffix, class_name, target) {
+		$(target).prepend('<img src="imgs/' + image_name + '.' + suffix + '" class="' + class_name + '" id="' + image_name + '"/>')
+	};
+
+	View.prototype.displayStrategy = function(strategy) {
+		var strategy = $(strategy)[0].value
+		$('#' + strategy +'_text').show()
 	};
 }
